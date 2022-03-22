69234f17f438dad60af3c83ead58beaab01de570
jayounglee <skrzk123@naver.com>
2022-03-22 17:48:49 +0900

    2진수에서 10진수 변환 수정

--- 2진수에서 10진수 변환.cpp
+++ 2진수에서 10진수 변환.cpp
@@ -3,19 +3,20 @@
 int main()
 {
 	int decimal=0;
-	int binary[4]={1,0,1,1};
-	int i =3;
-	while (i>=0){
+	int binary[4]={1,1,0,1};
+	int i =0;
+	while (i<=3){
 
 		if (binary[i] ==1){
 			int result=1;
-			for (int j=i-1;j>=0;j--){
+			for (int j=2-i;j>=0;j--){
 				result*=2;
 			}
 		decimal+=1*result;
 	}
-	i--;
+	i++;
 		}
 	printf("%d\n",decimal);
 	return 0;
-}
+	
+	}
